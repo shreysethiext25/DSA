08556e026516789a1341c29f8eed949c15b0e448
shreysethiext25 <shrey.sethi.ext@barco.com>
2024-06-12 15:58:00 +0530

    Create SelectionSort.c

---
+++ SelectionSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void selectionSort(int arr[], int n)
+{
+    int i, j, min;
+
+    for (i = 0; i < n-1; i++)
+    {
+        min = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min])
+            min = j;
+           if(min != i)
+            swap(&arr[min], &arr[i]);
+    }
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+
+    int i;
+    for (i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
